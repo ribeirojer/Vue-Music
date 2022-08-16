bb1c0fd6a4a69204adee93fe095c5469473b3f52
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2022-08-15 23:49:01 -0300

    feat(header): first version of header

---
+++ music/src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="header">
+        <img src="" alt="">
+        <nav>
+            <ul>
+                <li>Placas</li>
+                <li>Decorativos</li>
+                <li>Ferramentas</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    height: 4rem;
+    background-color: #ff0;
+}
+</style>
--- music/src/components/HelloWorld.vue
+++ music/src/components/HelloWorld.vue
@@ -9,8 +9,8 @@ const count = ref(0)
 </script>
 
 <template>
+<div>
   <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
@@ -31,7 +31,7 @@ const count = ref(0)
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
+</div></template>
 
 <style scoped>
 .read-the-docs {
